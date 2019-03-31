6e9b06df9e705c54c0cd40af94a13103be87adf9
cmt0726 <44270704+cmt0726@users.noreply.github.com>
2019-03-31 12:29:48 -0700

    Added Zipcode address functionality

--- websiteV3/scripts/zipsearch.js
+++ websiteV3/scripts/zipsearch.js
@@ -1,19 +1,4 @@
-function codeAddress() {
-    geocoder.geocode({
-      componentRestrictions: {
-        country: 'AU',
-        postalCode: '2000'
-      }
-    }, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
-        });
-      } else {
-        window.alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-    }
-codeAddress();
+
+
+
+//codeAddress();
